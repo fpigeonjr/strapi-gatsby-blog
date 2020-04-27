c614341304fbf116b8b8bb1d2b0b28c0b44f777f
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-04-27 07:59:00 -0500

    adds UIkit cdn and Staatliches font

--- frontend/src/components/seo.js
+++ frontend/src/components/seo.js
@@ -34,6 +34,30 @@ function SEO({ description, lang, meta, title }) {
       }}
       title={title}
       titleTemplate={`%s | ${site.siteMetadata.title}`}
+      link={[
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css?family=Staatliches",
+        },
+        {
+          rel: "stylesheet",
+          href:
+            "https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/css/uikit.min.css",
+        },
+      ]}
+      script={[
+        {
+          src:
+            "https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/js/uikit.min.js",
+        },
+        {
+          src:
+            "https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/js/uikit-icons.min.js",
+        },
+        {
+          src: "https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/js/uikit.js",
+        },
+      ]}
       meta={[
         {
           name: `description`,
