8352850c03734663f790206601be21f745baf657
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-04-27 11:23:25 -0500

    adds routes to categories

--- frontend/gatsby-node.js
+++ frontend/gatsby-node.js
@@ -18,6 +18,13 @@ exports.createPages = async ({ graphql, actions }) => {
             }
           }
         }
+        categories: allStrapiCategory {
+          edges {
+            node {
+              strapiId
+            }
+          }
+        }
       }
     `
   )
@@ -26,12 +33,24 @@ exports.createPages = async ({ graphql, actions }) => {
   }
 
   const articles = result.data.articles.edges
+  const categories = result.data.categories.edges
+
   articles.forEach((article, index) => {
     createPage({
       path: `/article/${article.node.strapiId}`,
       component: require.resolve("./src/templates/article.js"),
       context: {
         id: article.node.strapiId,
+      },
+    })
+  })
+
+  categories.forEach((category, index) => {
+    createPage({
+      path: `/category/${category.node.strapiId}`,
+      component: require.resolve("./src/templates/category.js"),
+      context: {
+        id: category.node.strapiId,
       },
     })
   })
