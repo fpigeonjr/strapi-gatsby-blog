e65d01f60d469f05620f012529db79f8915b3172
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-04-27 08:22:42 -0500

    adds env for dev

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -1,3 +1,7 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: "My super blog",
@@ -16,7 +20,7 @@ module.exports = {
     {
       resolve: "gatsby-source-strapi",
       options: {
-        apiURL: "http://localhost:1337",
+        apiURL: process.env.API_URL || "http://localhost:1337",
         contentTypes: [
           // List of the Content Types you want to be able to request from Gatsby.
           "article",
