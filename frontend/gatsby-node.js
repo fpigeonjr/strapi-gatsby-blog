304f8522711d5a252405f698630f3809db6e7038
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-04-27 10:49:53 -0500

    adds adds routing from home to article detail

--- frontend/gatsby-node.js
+++ frontend/gatsby-node.js
@@ -5,3 +5,34 @@
  */
 
 // You can delete this file if you're not using it
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const result = await graphql(
+    `
+      {
+        articles: allStrapiArticle {
+          edges {
+            node {
+              strapiId
+            }
+          }
+        }
+      }
+    `
+  )
+  if (result.errors) {
+    throw result.errors
+  }
+
+  const articles = result.data.articles.edges
+  articles.forEach((article, index) => {
+    createPage({
+      path: `/article/${article.node.strapiId}`,
+      component: require.resolve("./src/templates/article.js"),
+      context: {
+        id: article.node.strapiId,
+      },
+    })
+  })
+}
